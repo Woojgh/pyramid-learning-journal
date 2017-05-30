22947f46773abd76836b9edaeb02570390436067
Miguel Pena <miguelp1986@gmail.com>
2017-05-30 16:03:56 -0700

    completed main funtionality

--- python_learning_journal/python_learning_journal/views/default.py
+++ python_learning_journal/python_learning_journal/views/default.py
@@ -1,33 +1,34 @@
+""""I don't know what I'm doing."""
 from pyramid.response import Response
-from pyramid.view import view_config
-
-from sqlalchemy.exc import DBAPIError
-
-from ..models import MyModel
+import io
+import os
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'Python Learning Journal'}
+HERE = os.path.dirname(__file__)
+
+def list_view(request):
+    with io.open(os.path.join(HERE, '../templates/entry.html')) as the_file:
+        imported_text = the_file.read()
+
+    return Response(imported_text)
 
 
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+def detail_view(request):
+    with io.open(os.path.join(HERE, '../templates/entry.html')) as the_file:
+        imported_text = the_file.read()
 
-1.  You may need to run the "initialize_python_learning_journal_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
+    return Response(imported_text)
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
 
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+def create_view(request):
+    with io.open(os.path.join(HERE, '../templates/new.html')) as the_file:
+        imported_text = the_file.read()
+
+    return Response(imported_text)
+
+
+def update_view(request):
+    with io.open(os.path.join(HERE, '../templates/edit.html')) as the_file:
+        imported_text = the_file.read()
+
+    return Response(imported_text)
