73e8afd44a8d4ffbbdebf43d5a0671b8acec86a5
Woojgh <Woojgh>
2017-06-03 16:12:21 -0700

    heroku postgres database

--- python_learning_journal/python_learning_journal/__init__.py
+++ python_learning_journal/python_learning_journal/__init__.py
@@ -1,11 +1,12 @@
-from pyramid.config import Configurator
 import os
+from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
+    if os.environ.get('DATABASE_URL', ''):
+        settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
