d86df1ae14ab1931dc73fd3481f8467b0c11bd4d
Miguel Pena <miguelp1986@gmail.com>
2017-06-04 13:10:27 -0700

    importing modules not working for webtest and dict from data directory. Lovely

--- python_learning_journal/python_learning_journal/tests.py
+++ python_learning_journal/python_learning_journal/tests.py
@@ -1,6 +1,17 @@
 from pyramid import testing
 from pyramid.response import Response
+from python_learning_journal.views.default import JOURNAL_ENTRIES_DICT
 import pytest
+
+
+@pytest.fixture
+def testapp():
+    """Create an instance of our app for testing."""
+    from python_learning_journal import main
+    app = main({})
+    from webtest import TestApp
+    return TestApp(app)
+
 
 @pytest.fixture
 def list_view_response():
@@ -34,107 +45,46 @@ def update_view_response():
     return update_view(request)
 
 
-def test_list_view_returns_response_given_request(list_view_response):
+def test_list_view_returns_dict_given_request(list_view_response):
     """Assert if list view returns a valid response."""
-    assert isinstance(list_view_response, Response)
+    assert isinstance(list_view_response, dict)
 
 
-def test_detail_view_returns_response_given_request(detail_view_response):
+def test_detail_view_returns_dict_given_request(detail_view_response):
     """Assert if detail view returns a valid response."""
-    assert isinstance(detail_view_response, Response)
+    assert isinstance(detail_view_response, dict)
 
 
-def test_create_view_returns_response_given_request(create_view_response):
+def test_create_view_returns_dict_given_request(create_view_response):
     """Assert if create view returns a valid response."""
-    assert isinstance(create_view_response, Response)
+    assert isinstance(create_view_response, dict)
 
 
-def test_update_view_returns_response_given_request(update_view_response):
+def test_update_view_returns_dict_given_request(update_view_response):
     """Assert if update view returns a valid response."""
-    assert isinstance(update_view_response, Response)
+    assert isinstance(update_view_response, dict)
 
 
-def test_list_view_is_good(list_view_response):
-    """Assert that list view response returns a status 200 OK."""
-    assert list_view_response.status_code == 200
+def test_list_view_returns_proper_len_of_content(list_view_response):
+    """Assert list view returns proper length of content."""
+    assert len(list_view_response.get('journal_entries')) == len(JOURNAL_ENTRIES_DICT)
 
 
-def test_detail_view_is_good(detail_view_response):
-    """Assert that detail view response returns a status 200 OK."""
-    assert detail_view_response.status_code == 200
+def test_detail_view_contains_journal_entry_attrs(detail_view_response):
+    """Asser that detail view response contains correct data."""
+    for key in ['id', 'title', 'creation_date', 'body']:
+        assert key in detail_view_response.keys()
 
 
-def test_create_view_is_good(create_view_response):
-    """Assert that create view response returns a status 200 OK."""
-    assert create_view_response.status_code == 200
+def test_layout(testapp):
+    """Assert that layout file contains correct data."""
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert 'JourNull' in html.find('h1').text
 
 
-def test_update_view_is_good(update_view_response):
-    """Assert that update view response returns a status 200 OK."""
-    assert update_view_response.status_code == 200
-
-
-def test_list_view_returns_proper_content(list_view_response):
-    """Assert that List view response has proper contnent."""
-    expected_text = """<nav class="main-nav">
-        <div class="icon-menu"></div>
-        <ul>
-          <li class="tab" data-content="entries"><a href="/" class="icon-home"> Home</a></li>
-          <li class="tab"><a href="/journal/new-entry">New Entry</a></li>
-          <li class="tab" data-content="about"><a href="/about" class="icon-address-book"> About</a></li>
-        </ul>
-      </nav>"""
-    assert expected_text in list_view_response.text
-
-
-def test_detail_vieiw_returns_proper_content(detail_view_response):
-    """Assert that Detail view response has proper content."""
-    expected_text = """<nav class="main-nav">
-        <div class="icon-menu"></div>
-        <ul>
-          <li class="tab" data-content="entries"><a href="/" class="icon-home"> Home</a></li>
-          <li class="tab"><a href="/journal/new-entry">New Entry</a></li>
-          <li class="tab" data-content="about"><a href="/about" class="icon-address-book"> About</a></li>
-        </ul>
-      </nav>"""
-    assert expected_text in detail_view_response.text
-
-
-def test_create_view_returns_proper_content(create_view_response):
-    """Assert that create view has proper content."""
-    expected_text = """<section id="write" class="tab-content">
-        <h1>New Entrees</h1>
-        <form action="#" id="new-form">
-          <label>
-            <input type="text" id="entry-title" placeholder="Entry title" required>
-          </label>
-          <textarea id="entry-body" rows="8" cols="40" required></textarea>
-          <label>
-            <input type="text" id="entry-author" placeholder="Author Name" required>
-          </label>
-          <label>
-            <input type="text" id="entry-author-url" placeholder="Author's URL" required>
-          </label>
-          <label>
-            <input type="text" id="entry-category" placeholder="Category" required>
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-      </section>"""
-    assert expected_text in create_view_response.text
-
-
-def test_update_view_returns_proper_content(update_view_response):
-    """Asser that update view has proper content."""
-    expected_text = """<main class="clearfix">
-      <h1>Blog Stats</h1>
-      <section id="blog-stats">
-        <dt>Total entries:</dt> <dd class="entries"></dd>
-        <dt>Total words:</dt> <dd class="words"></dd>
-        <h2>Author Stats</h2>
-        <p><em>Details on who is writing, and how much writing they are doing...</em></p>
-        <ul class="author-stats">
-        </ul>
-      </section>
-    </main>"""
-    assert expected_text in update_view_response.text
+def test_root_contents(testapp):
+    """Assert that listing view contais the correct ammount of article tags."""
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(JOURNAL_ENTRIES_DICT) == len(html.finaAll('article'))
