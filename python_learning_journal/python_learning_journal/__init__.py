66d6279cb4ff0f237abde935505082dd03c5d65c
Woojgh <Woojgh>
2017-06-08 22:28:18 -0700

    heroku stuff

--- python_learning_journal/python_learning_journal/__init__.py
+++ python_learning_journal/python_learning_journal/__init__.py
@@ -1,10 +1,16 @@
 from pyramid.config import Configurator
 import os
+import twitter
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    api = twitter.Api(consumer_key='consumer_key',
+                      consumer_secret='consumer_secret',
+                      access_token_key='access_token',
+                      access_token_secret='access_token_secret')
+    print(api.VerifyCredentials())
     settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
