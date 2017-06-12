d4aff563f194bb909ae57790cfdbb89ac1e2c932
Woojgh <Woojgh>
2017-06-11 17:13:35 -0700

    getting security working again

---
+++ python_learning_journal/python_learning_journal/scripts/initializedb.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import transaction
+import datetime
+
+from pyramid.paster import (
+    get_appsettings,
+    setup_logging,
+    )
+
+from pyramid.scripts.common import parse_vars
+
+from ..models.meta import Base
+from ..models import (
+    get_engine,
+    get_session_factory,
+    get_tm_session,
+    )
+from python_learning_journal.models import Entry
+from python_learning_journal.data.data import JOURNAL_ENTRIES
+
+
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri> [var=value]\n'
+          '(example: "%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
+
+
+def main(argv=sys.argv):
+    if len(argv) < 2:
+        usage(argv)
+    config_uri = argv[1]
+    options = parse_vars(argv[2:])
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri, options=options)
+    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
+
+    engine = get_engine(settings)
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    session_factory = get_session_factory(engine)
+
+    with transaction.manager:
+        dbsession = get_tm_session(session_factory, transaction.manager)
+        entries = []
+
+        for item in JOURNAL_ENTRIES:
+            new_entry = Entry(
+                title=item['title'],
+                body=item['body'],
+                creation_date=datetime.datetime.now()
+            )
+            entries.append(new_entry)
+
+        dbsession.add_all(entries)
---
+++ python_learning_journal/python_learning_journal/views/default.py
@@ -0,0 +1,119 @@
+"""Create views and route them to jinja2 files."""
+from pyramid.view import view_config
+from pyramid.httpexceptions import (
+    HTTPNotFound,
+    HTTPFound
+)
+from python_learning_journal.models import Entry
+import datetime
+from pyramid.security import remember, forget
+from python_learning_journal.security import check_credentials
+
+
+@view_config(route_name='list_view', renderer='../templates/listing.jinja2', require_csrf=False)
+def list_view(request):
+    """View for the main listing page."""
+    journal_entries = request.dbsession.query(Entry).all()
+    return {"journal_entries": journal_entries}
+
+
+@view_config(route_name='detail_view', renderer='../templates/detail.jinja2', require_csrf=False)
+def detail_view(request):
+    """Detailed view for one journal entry based on it's id."""
+    the_id = int(request.matchdict['id'])
+    entry = request.dbsession.query(Entry).get(the_id)
+
+    if not entry:
+        raise HTTPNotFound
+
+    if request.method == 'GET':
+        return {
+            'entry': entry
+        }
+
+    if request.method == 'POST':
+        return HTTPFound(
+            location=request.route_url('update_view', id=entry.id)
+        )
+
+    return {}
+
+
+@view_config(route_name='create_view', renderer='../templates/create.jinja2', permission='secret', require_csrf=True)
+def create_view(request):
+    """View for new listing route."""
+    if request.method == "POST":
+        if not request.POST['title'] or not request.POST['body']:
+            return {
+                'title': request.POST['title'],
+                'body': request.POST['body']
+            }
+
+        new_entry = Entry(
+            title=request.POST['title'],
+            body=request.POST['body'],
+            creation_date=datetime.datetime.now()
+        )
+
+        request.dbsession.add(new_entry)
+        return HTTPFound(
+            location=request.route_url('list_view')
+        )
+
+    return {}
+
+
+@view_config(route_name='update_view', renderer='../templates/update.jinja2', permission='secret', require_csrf=True)
+def update_view(request):
+    """View to make changes to a journal entry."""
+    the_id = int(request.matchdict['id'])
+    entry = request.dbsession.query(Entry).get(the_id)
+
+    if not entry:
+        raise HTTPNotFound
+
+    if request.method == 'GET':
+        return {
+            'title': entry.title,
+            'body': entry.body
+        }
+
+    if request.method == 'POST':
+        entry.title = request.POST['title']
+        entry.body = request.POST['body']
+        request.dbsession.flush()
+
+        return HTTPFound(
+            location=request.route_url('detail_view', id=entry.id)
+        )
+
+    return {}
+
+
+@view_config(route_name='login_view', renderer='../templates/login.jinja2', require_csrf=False)
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if check_credentials(username, password):
+            headers = remember(request, username)
+            return HTTPFound(
+                location=request.route_url('list_view'),
+                headers=headers
+            )
+
+        else:
+            return {'error': 'Bad request'}
+    return {}
+
+
+@view_config(route_name='logout_view', require_csrf=False)
+def logout(request):
+    headers = forget(request)
+    return HTTPFound(request.route_url('list_view'), headers=headers)
+
+
+@view_config(route_name='api_journal_list', renderer='json')
+def api_list(request):
+    entries = request.dbsession.query(Entry).all()
+    return {'entries': entries}
