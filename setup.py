fb751f8e3ea57541d65ecd1b36b15acb6d0dd284
Woojgh <jamessalamonsen@gmail.com>
2017-06-18 19:37:12 -0700

    adding tox

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'python_learning_journal/README.txt')) as f:
     README = f.read()
 with open(os.path.join(here, 'python_learning_journal/CHANGES.txt')) as f:
     CHANGES = f.read()
